3684a9bef9f96166a8eb3965486254f91d2b3f8f
Labib (Md. Al Shahriar Labib) <20260149@citybankplc.com>
2026-02-09 17:24:51 +0600

    check

---
+++ src/main/java/com/taskmanagementsystem/EmployeeTaskManagementSystem/EmployeeTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanagementsystem.EmployeeTaskManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanagementsystem/EmployeeTaskManagementSystem/EmployeeTaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanagementsystem.EmployeeTaskManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
